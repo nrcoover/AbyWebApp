dfbfeaa5b23a07e686a7c033cfd23bd4678e7b92
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-04 11:48:42 -0400

    Configures Connection to SQL Server
    Configures the application to work with the .NET Entity Framework Core to connect to the SQL Server.  Adds builder services in Program.cs and creates our DbContext connection in our ApplicationDbContext class.

---
+++ AbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
