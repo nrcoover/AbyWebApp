97c20c8992f8f619fef3f5917e787b6b64be0c48
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-05-04 23:31:32 -0400

    Adds New Pages and Connectivity Between Them
    Creates new Category EDIT and Category DELETE pages from the Create Page Model. Adds connectivity between pages as well as ability to connect with and edit the database.

---
+++ AbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using AbyWeb.Data;
+using AbyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+//Used if multiple prorerties need binding in the future.
+//[BindProperties]
+namespace AbyWeb.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Categories.Find(id);
+            //Category = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category = _db.Categories.SingleOrDefault(u => u.Id == id);
+            //Category = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var categoryFromDb = _db.Categories.Find(Category.Id);
+            if(categoryFromDb != null)
+            {
+                _db.Categories.Remove(categoryFromDb);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
---
+++ AbyWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,46 @@
+using AbyWeb.Data;
+using AbyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+//Used if multiple prorerties need binding in the future.
+//[BindProperties]
+namespace AbyWeb.Pages.Categories
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            Category = _db.Categories.Find(id);
+            //Category = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category = _db.Categories.SingleOrDefault(u => u.Id == id);
+            //Category = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The Display Name value cannot exactly match the Name value.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
